e217b5cf4baa1bbce9d5178cca641b02c24a043a
harryzhangpro <harryzhangpro@gmail.com>
2024-08-30 10:53:43 +0800

    modified:   Wukong.CT 	modified:   dglab.py

--- dglab.py
+++ dglab.py
@@ -1,16 +1,16 @@
 import websockets
 import json
 
-async def send_message():
+async def send_message(ticks):
     # 连接到WebSocket服务器
     uri = "ws://192.168.31.61:60536/1" # IP地址改这里
     async with websockets.connect(uri) as websocket:
         # 要发送的数据
-        message = {
-            "cmd": "set_pattern",
-            "pattern_name": "经典",
-            "intensity": 100,
-            "ticks": 30
+        message = { 
+            "cmd": "set_pattern",  
+            "A_pattern_name": "经典",  
+            "A_intensity": 100, 
+            "A_ticks": 30  
         }
         
         # 将Python字典转换为JSON字符串
