1e1a60cc3f3e8ad95525a0614704305337c6fd76
harryzhangabc <503433013@qq.com>
2024-09-02 12:58:23 +0800

    modified:   dglab.py

--- dglab.py
+++ dglab.py
@@ -6,7 +6,7 @@ async def send_message(intensity, ticks):
     uri = "ws://192.168.31.61:60536/1"  # IP地址改这里
     message = {  
         "cmd": "set_pattern",  
-        "pattern_name": "经典",  
+        "pattern_name": "冲击",  
         "intensity": intensity,  
         "ticks": ticks  
     }
@@ -14,14 +14,9 @@ async def send_message(intensity, ticks):
     json_message = json.dumps(message)
     
     try:
-        # 使用 asyncio.wait_for 包装连接操作，以设置连接超时
-        websocket = await asyncio.wait_for(websockets.connect(uri), timeout=0.10)
-        async with websocket:
-            try:
-                await asyncio.wait_for(websocket.send(json_message), timeout=0.10)  # 设置发送超时
-                print(f"Sent: {json_message}")
-            except asyncio.TimeoutError:
-                print("Error: Timed out waiting for a response from the server.")
+        async with websockets.connect(uri) as websocket:
+            await websocket.send(json_message)
+            print(f"Sent: {json_message}")
             
             try:
                 response = await asyncio.wait_for(websocket.recv(), timeout=0.10)  # 设置接收超时
@@ -30,9 +25,8 @@ async def send_message(intensity, ticks):
                 print("Error: Timed out waiting for a response from the server.")
             
     except asyncio.TimeoutError:
-        print("Error: Connection timed out while trying to connect to the server.")
+        print("Error: Connection timed out while trying to send the message.")
     except websockets.exceptions.ConnectionClosedError as e:
         print(f"Error: Connection closed with error: {e}")
     except Exception as e:
         print(f"Error: Failed to send message due to: {e}")
-
